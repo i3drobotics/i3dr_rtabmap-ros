d569bebf789aba616b56ee7b9652353ca2085819
IIND Cheaper <wood207467@gmail.com>
2019-09-15 08:41:10 +0100

    rtabmap control

--- src/i3dr_rtabmap_transfer.py
+++ src/i3dr_rtabmap_transfer.py
@@ -14,6 +14,9 @@ class RtabmapTransfer:
     
         rospy.init_node("i3dr_rtabmap_transfer", anonymous=True, disable_signals=True)
         rospy.Service('i3dr_scan_send_map', Empty, self.handle_i3dr_scan_send_map)
+        rospy.Service('i3dr_scan_pause', Empty, self.handle_i3dr_scan_pause)
+        rospy.Service('i3dr_scan_resume', Empty, self.handle_i3dr_scan_resume)
+        rospy.Service('i3dr_scan_new_map', Empty, self.handle_i3dr_scan_new_map)
 
         self.rtabmap_namespace = rospy.get_param('~rtabmap_namespace', "rtabmap")
 
@@ -51,6 +54,98 @@ class RtabmapTransfer:
         except:
             rospy.logerr("Service call failed: Unknown error")
 
+    def rtabmap_pause(self):
+        print "Pausing rtabmap with service: /"+self.rtabmap_namespace+"/pause"
+        try:
+            rospy.wait_for_service(
+                '/'+self.rtabmap_namespace+'/pause', timeout=2)
+            srv_pause = rospy.ServiceProxy(
+                '/'+self.rtabmap_namespace+'/pause', Empty)
+            srv_pause()
+            self.rtabmap_is_paused = True
+            print "Rtabmap service complete: /"+self.rtabmap_namespace+"/pause"
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s", e)
+        except rospy.ROSException, e:
+            rospy.logerr("Service call failed: %s", e)
+
+    def rtabmap_pause_odom(self):
+        print "Pausing rtabmap odometry with service: /" + \
+            self.rtabmap_namespace+"/pause_odom"
+        try:
+            rospy.wait_for_service(
+                '/'+self.rtabmap_namespace+'/pause_odom', timeout=2)
+            srv_pause_odom = rospy.ServiceProxy(
+                '/'+self.rtabmap_namespace+'/pause_odom', Empty)
+            srv_pause_odom()
+            self.rtabmap_is_paused = True
+            print "Rtabmap service complete: /"+self.rtabmap_namespace+"/pause_odom"
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s", e)
+        except rospy.ROSException, e:
+            rospy.logerr("Service call failed: %s", e)
+
+    def rtabmap_resume(self):
+        try:
+            rospy.wait_for_service(
+                '/'+self.rtabmap_namespace+'/resume', timeout=2)
+            srv_resume = rospy.ServiceProxy(
+                '/'+self.rtabmap_namespace+'/resume', Empty)
+            srv_resume()
+            self.rtabmap_is_paused = False
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s", e)
+        except rospy.ROSException, e:
+            rospy.logerr("Service call failed: %s", e)
+
+    def rtabmap_resume_odom(self):
+        try:
+            rospy.wait_for_service(
+                '/'+self.rtabmap_namespace+'/resume_odom', timeout=2)
+            srv_reset = rospy.ServiceProxy(
+                '/'+self.rtabmap_namespace+'/resume_odom', Empty)
+            srv_reset()
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s", e)
+        except rospy.ROSException, e:
+            rospy.logerr("Service call failed: %s", e)
+
+    def rtabmap_reset_odom(self):
+      try:
+          rospy.wait_for_service(
+                '/'+self.rtabmap_namespace+'/reset_odom', timeout=2)
+          srv_reset_odom = rospy.ServiceProxy(
+                '/'+self.rtabmap_namespace+'/reset_odom', Empty)
+          srv_reset_odom()
+      except rospy.ServiceException, e:
+          rospy.logerr("Service call failed: %s", e)
+      except rospy.ROSException, e:
+          rospy.logerr("Service call failed: %s", e)
+
+    def rtabmap_new_map(self):
+        try:
+            rospy.wait_for_service(
+                '/'+self.rtabmap_namespace+'/trigger_new_map', timeout=2)
+            srv_new_map = rospy.ServiceProxy(
+                '/'+self.rtabmap_namespace+'/trigger_new_map', Empty)
+            srv_new_map()
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s", e)
+        except rospy.ROSException, e:
+            rospy.logerr("Service call failed: %s", e)
+
+    def rtabmap_reset(self):
+        try:
+            rospy.wait_for_service(
+                '/'+self.rtabmap_namespace+'/reset', timeout=2)
+            srv_reset = rospy.ServiceProxy(
+                '/'+self.rtabmap_namespace+'/reset', Empty)
+            srv_reset()
+        except rospy.ServiceException, e:
+            rospy.logerr("Service call failed: %s", e)
+        except rospy.ROSException, e:
+            rospy.logerr("Service call failed: %s", e)
+
     def handle_i3dr_scan_send_map(self,req):
         print("request to send latest map")
         self.rtabmap_get_map()
@@ -59,6 +154,23 @@ class RtabmapTransfer:
         else:
             rospy.logerr("Unable to send map. No data found in map!")
         return EmptyResponse()
+
+    def handle_i3dr_scan_pause(self, req):
+          self.rtabmap_pause()
+          self.rtabmap_pause_odom()
+          return EmptyResponse()
+
+    def handle_i3dr_scan_resume(self, req):
+          self.rtabmap_resume()
+          self.rtabmap_resume_odom()
+          return EmptyResponse()
+
+    def handle_i3dr_scan_new_map(self, req):
+          self.rtabmap_new_map()
+          self.rtabmap_reset()
+          self.rtabmap_reset_odom()
+          self.rtabmap_get_map()
+          return EmptyResponse()
 
 if __name__ == '__main__':
     rt = RtabmapTransfer()
